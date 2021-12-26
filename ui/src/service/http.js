8fca4a7d073cdad720dea8ef33a46ef592e53abf
Egor Krivosheev <egor.krv@gmail.com>
2021-12-26 20:09:18 +1100

    remove auth from HTTP service in UI

--- ui/src/service/http.js
+++ ui/src/service/http.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 export const HTTP = axios.create({
   baseURL: `https://127.0.0.1:1443/v1/`,
-  auth: {
-    username: "someuser",
-    password: "somepasss"
-  }
+  // need for UI development  
+  // auth: {
+  //   username: "",
+  //   password: ""
+  // }
 });
