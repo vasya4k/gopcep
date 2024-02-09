a1cc2b364680a115c06802a8933b7ecedab7a536
Egor Krivosheev <egor.krv@gmail.com>
2024-02-09 22:05:21 +1100

    remove basic auth

--- ui/src/service/http.js
+++ ui/src/service/http.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 export const HTTP = axios.create({
     baseURL: `https://127.0.0.1:1443/v1/`,
     // need for UI development
-    auth: {
-        username: 'admin',
-        password: 'pass'
-    }
+    // auth: {
+    //     username: 'admin',
+    //     password: 'pass'
+    // }
 });
