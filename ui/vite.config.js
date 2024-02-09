5ad54352131b4b2445d89835c457750fe4da258c
Egor Krivosheev <egor.krv@gmail.com>
2024-02-09 21:04:12 +1100

    change base path

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -7,6 +7,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig(() => {
     return {
         plugins: [vue()],
+        base: '/ui/static/',
         resolve: {
             alias: {
                 '@': fileURLToPath(new URL('./src', import.meta.url))
